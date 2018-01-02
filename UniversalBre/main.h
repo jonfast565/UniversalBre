ae6320240ba103477d150fcafa15f5a91b2cad9f
Jon Fast <jnfstdj656@gmail.com>
2018-01-01 20:58:21 -0500

    interactive mode.

--- UniversalBre/main.h
+++ UniversalBre/main.h
@@ -1,5 +1,7 @@
 #pragma once
 
-void interactive_wait();
-void print_header();
-void interactive_main(core::log* l, core::console_handler* h);
+namespace {
+    void interactive_wait();
+    void print_header();
+    void interactive_main(core::log* l);
+}
