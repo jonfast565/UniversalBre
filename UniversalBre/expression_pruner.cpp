b5477d4ba87371781fcf57c94d2397b11e1f5838
Jon Fast <jnfstdj656@gmail.com>
2018-01-16 16:05:12 -0500

    add expression pruning code.

---
+++ UniversalBre/expression_pruner.cpp
@@ -0,0 +1,6 @@
+#include "expression_pruner.h"
+
+core::expression_node_ptr_s core::expression_pruner::prune(core::expression_node_ptr_s expression_root)
+{
+
+}
---
+++ UniversalBre/expression_pruner.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "expression_node.h"
+#include "binop_expression_node.h"
+#include "literal_expression_node.h"
+#include "singleop_expression_node.h"
+
+namespace core {
+    class expression_pruner
+    {
+    public:
+        expression_pruner() { }
+        ~expression_pruner() { }
+        expression_node_ptr_s prune(expression_node_ptr_s expression_root);
+    };
+}
+
