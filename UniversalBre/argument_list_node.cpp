af741dd7d93c735b61733326e44635c52ff97e57
Jon Fast <jnfstdj656@gmail.com>
2018-02-04 16:04:11 -0500

    added some items from CMake to gitignore. Added argument_list_node initializer for internal vector.

--- UniversalBre/argument_list_node.cpp
+++ UniversalBre/argument_list_node.cpp
@@ -1,7 +1,9 @@
 #include "argument_list_node.h"
+#include "utility.h"
 
 core::argument_list_node::argument_list_node()
 {
+    arguments_ = utility::make_ptr_s(token_vecptr_s());
 }
 
 void core::argument_list_node::add_argument(const token_ptr_s argument) const
