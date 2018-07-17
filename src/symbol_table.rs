5e356eed12326985b915271e33adaaf0bca97cb9
Jon Fast <jfast@acc.org>
2018-07-17 12:44:39 -0400

    added more simple methods

--- src/symbol_table.rs
+++ src/symbol_table.rs
@@ -2,25 +2,34 @@ use std::collections::HashMap;
 
 pub struct SymbolRecord {
     id: String,
-    offset: u8
+    offset: u8,
+    scope: u8
 }
 
 pub struct SymbolTable {
-    records: HashMap<String, SymbolRecord> //TODO: Use a map type if possible
+    records: HashMap<String, SymbolRecord>, //TODO: Use a map type if possible
+    offset: u8
 }
 
 impl SymbolTable {
     pub fn init() -> SymbolTable {
         SymbolTable {
-            records: HashMap::<String, SymbolRecord>::new()
+            records: HashMap::<String, SymbolRecord>::new(),
+            offset: 0
         }
     }
 
-    pub fn add_record(&mut self, id: String, name: String, offset: u8) {
+    pub fn add_record(&mut self, id: String, name: String, scope: u8) {
         self.records.insert(name, SymbolRecord{
             id: id,
-            offset: offset
+            offset: self.offset,
+            scope: scope
         });
+        self.offset += 1;
+    }
+
+    pub fn search_record(&mut self, name: String) -> Option<&SymbolRecord> {
+        self.records.get(&name)
     }
 }
 
